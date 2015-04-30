0e32e746bcd4bc3fb125469318b4588dee1b869b
johanreimann <reimann.johan@gmail.com>
2015-04-30 12:45:49 +0200

    table with 36 cards

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,10 +1,66 @@
 @extends('master')
 
 @section('content')
-<h1>About me {{ $name }}</h1>
 
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit venenatis ante, vitae accumsan odio rhoncus nec. Morbi commodo risus in placerat posuere. Donec at odio dignissim, ullamcorper nulla ut, lacinia risus. Nullam lectus dui, euismod at malesuada vitae, faucibus sed eros. Donec pellentesque egestas tortor, a finibus ipsum. Aliquam erat volutpat. Sed sagittis mauris rhoncus fringilla luctus. Aliquam maximus sit amet sem laoreet scelerisque. Aenean pharetra arcu urna, vitae sagittis ligula porttitor nec. Pellentesque pellentesque consequat sem, sit amet placerat diam molestie dapibus. Sed vitae rhoncus nisl, vitae efficitur sem. Nulla ut facilisis mauris. Integer tempor urna congue nulla tristique, id vehicula nibh suscipit.
-</p>
+<center>
+	<form name=game>
+		<table border=0>
+			<td>
+				<table border=1>
+					<tr>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=1></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=2></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=3></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=4></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=5></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=6></a></td>
+					</tr>
+					<tr>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=7></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=8></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=9></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=10></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=11></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=12></a></td>
+					</tr>
+					<tr>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=13></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=14></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=15></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=16></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=17></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=18></a></td>
+					</tr>
+					<tr>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=19></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=20></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=21></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=22></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=23></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=24></a></td>
+					</tr>
+					<tr>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=25></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=26></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=27></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=28></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=29></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=30></a></td>
+					</tr>
+					<tr>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=31></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=32></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=33></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=34></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=35></a></td>
+						<td><a><img src="{{asset('images/tweety.png')}}" border=0 height=150 width=150 name=36></a></td>
+					</tr>
+					</table>
+					</td>
+					<td>
+			</td>
+		</table>
+	</form>
+</center>
 
 @stop
