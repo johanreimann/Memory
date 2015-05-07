faf748ccf9aba4a549d74fce54301ed22bb2bd7c
Carl Englund <englund92@gmail.com>
2015-05-07 09:58:43 +0200

    Game works but seems rather buggy, not sure if it's my configuration or if there's a problem with something else.. Sorry huge commit. What was done for it in order to work was that we store variables in sessions, in this way we can transfer varibales from different methods in the controller. We then send an AJAX-request from the client side whenever an image is clicked, this is recieved and processed in the gameController and then we send back the correct information to the clientside. Still needs alot of work and refactoring. We probably should create some sort of API-controller that handles the AJAX requests seperated from the other controllers/methods

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,7 +2,9 @@
 use Input;
 use Response;
 use Request;
+use Session;
 
+ 
 
 class gameController extends Controller {
 
@@ -14,26 +16,50 @@ class gameController extends Controller {
 					   'images/Q.jpg', 'images/R.png', 'images/A.png', 'images/B.png', 'images/C.jpg', 'images/D.jpg', 'images/E.png', 'images/F.jpg', 
 					   'images/G.png', 'images/H.png', 'images/I.jpg', 'images/J.jpg', 'images/K.png', 'images/L.jpg', 'images/M.gif', 'images/N.gif', 
 					   'images/O.png', 'images/P.png', 'images/Q.jpg', 'images/R.png');
-		shuffle($pokes);
+		//shuffle($pokes);
+		Session::put('key', $pokes);
+		$count = 1;
+		$score = 0;
+		$check = 0;
+		Session::put('count', $count);
+		Session::put('score', $score);
+
+		Session:put('name', 'hej');
 
 		return view('game', compact('pokes'));
 	}
 
 	public function clicked()
 	{
-		 if(Request::ajax()) {
-     		 $data = Input::all();
-			  $pokes = array('images/A.png', 'images/B.png', 'images/C.jpg', 'images/D.jpg', 'images/E.png', 'images/F.jpg', 'images/G.png', 'images/H.png',
-					   'images/I.jpg', 'images/J.jpg', 'images/K.png', 'images/L.jpg', 'images/M.gif', 'images/N.gif', 'images/O.png', 'images/P.png',
-					   'images/Q.jpg', 'images/R.png', 'images/A.png', 'images/B.png', 'images/C.jpg', 'images/D.jpg', 'images/E.png', 'images/F.jpg', 
-					   'images/G.png', 'images/H.png', 'images/I.jpg', 'images/J.jpg', 'images/K.png', 'images/L.jpg', 'images/M.gif', 'images/N.gif', 
-					   'images/O.png', 'images/P.png', 'images/Q.jpg', 'images/R.png');
-			 
-			 
-     		 return response()->json(['name' => $pokes[intval($data['value'])]]);
-
-    }
+		$value = Session::get('key');
+		$count = Session::get('count');
+		$score = Session::get('score');
+		$name = Session::get('name');
+		$oldvalue = 0;
 		
+		 if(Request::ajax()) {
+			 $count++;
+     		 $data = Input::all();
+			 $oldvalue = $value[intval($name)];
+			 Session::put('name', $data['value']);
+			 $check = 0;
+			 if((strcmp($value[intval($data['value'])], $value[intval($name)]) == 0))
+			 {
+			 	$score++;
+				$check = 1;	
+			 }
+			 
+			 if($count == 2)
+			 	$count = 0;
+				 
+			 Session::put('count', $count);
+			 Session::put('score', $score);
+     		 return response()->json(['name' => $value[intval($data['value'])], 'count' => $count, 
+		  							  'score' => $score, 'test1' => $value[intval($data['value'])], 
+									  'test2' => $value[intval($name)], 'check' => $check, 
+									  'old' =>  $name]);
+	  		
+    	}	
 	}
 }
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,36 @@
+/* global imgs1 */
 $(document).ready(function(){
+    imgs1 = $(this).find('img');
   $('img').click(function(){   
     x = $(this).attr('name');
     c = $(this);
+  
+  	
     $.ajax({
       url: 'game',
       type: 'post',
         data: {'value': x, '_token': $('input[name=_token]').val()},
       success: function(data){
-         c.attr('src', data['name']);
+        c.attr('src', data['name']);
+       //console.log(data['check'])
+       //console.log(data['count']);
+       //console.log($(data['old']));
+        
+          	console.log(imgs1);
+           
+        if((data['count'] % 2) && (data['check'] == 0))
+        {
+          setTimeout(function() {
+            c.attr('src', 'images/tweety.png'); 
+            imgs1[data['old']-1].src = 'images/tweety.png';
+          }, 2000);
+        }
+         
+      
+           
       }
-    });      
+    });  
+
   }); 
+    
 });
